c8d90b98d4d815c341aa98f2148074cc851d9e2d
Helsa Sriprameswari Putri <144691463+helsasp@users.noreply.github.com>
2024-06-23 12:33:55 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -10,7 +10,8 @@ void main() {
 
 void printString(char* str) {
   while (*str != '\0') {
-    if (*str == '\n') interrupt(0x10, 0xE << 8 | '\r', 0, 0, 0);
+    if (*str == '\n') 
+      interrupt(0x10, 0xE << 8 | '\r', 0, 0, 0);
     interrupt(0x10, 0xE << 8 | *str, 0, 0, 0);
     str++;
   }
@@ -27,8 +28,7 @@ void readString(char* buf) {
         printString("\b \b");
         i--;
       }
-    }
-    else {
+    } else {
       buf[i++] = c;
       interrupt(0x10, 0xE << 8 | c, 0, 0, 0);
     }
@@ -72,4 +72,19 @@ void readSector(byte* buf, int sector) {
 }
 
 // TODO: 1. Implement writeSector function
-void writeSector(byte* buf, int sector) {}
+void writeSector(byte* buf, int sector) {
+  int ah = 0x03;                    // write sector service number
+  int al = 0x01;                    // number of sectors to write
+  int ch = div(sector, 36);         // cylinder number
+  int cl = mod(sector, 18) + 1;     // sector number
+  int dh = mod(div(sector, 18), 2); // head number
+  int dl = 0x00;                    // drive number
+
+  interrupt(
+    0x13,
+    ah << 8 | al,
+    buf,
+    ch << 8 | cl,
+    dh << 8 | dl
+  );
+}
